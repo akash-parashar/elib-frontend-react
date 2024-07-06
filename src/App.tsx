129fe1b424f65f08362d6148688a7db0bf5e9f18
akash-parashar <aparashar508@gmail.com>
2024-07-07 01:13:09 +0530

    add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 
-import './App.css'
+
+import { Button } from "./components/ui/button"
+
 
 function App() {
 
@@ -7,7 +9,8 @@ function App() {
   return (
     <>
       <h1>
-        Hello World
+      <Button variant="destructive">Button</Button>
+
       </h1>
     </>
   )
