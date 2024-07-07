9b7a23233b205752ba1b0bf4425ea9c78a7eb19f
akash-parashar <aparashar508@gmail.com>
2024-07-08 01:28:17 +0530

    persist access token in local storsage

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,17 +14,20 @@ import { useRef } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { login } from "../http/api";
 import { LoaderCircle } from "lucide-react";
+import useTokenStore from "../store";
 
 const LoginPage = () => {
   const navigate = useNavigate();
+  const setToken = useTokenStore((state)=>state.setToken)
 
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
   const mutation = useMutation({
     mutationFn: login,
-    onSuccess: () => {
+    onSuccess: (response) => {
       console.log("login succesfull");
+      setToken(response.data.accessToken)
       navigate("/dashboard/home");
     },
   });
