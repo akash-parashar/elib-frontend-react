dd87d52805e2e6bdda3a46d2f097632ccb2ebd59
akash-parashar <aparashar508@gmail.com>
2024-07-08 00:20:35 +0530

    add laoding state

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -12,15 +12,14 @@ import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
 import { useRef } from "react";
 import { useMutation } from "@tanstack/react-query";
-import { login} from "../http/api";
+import { login } from "../http/api";
+import { LoaderCircle } from "lucide-react";
 
 const LoginPage = () => {
   const navigate = useNavigate();
-  
+
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
-
-
 
   const mutation = useMutation({
     mutationFn: login,
@@ -41,8 +40,6 @@ const LoginPage = () => {
 
     //mutate
     mutation.mutate({ email, password });
-
-  
   };
 
   return (
@@ -52,6 +49,7 @@ const LoginPage = () => {
           <CardTitle className="text-2xl">Login</CardTitle>
           <CardDescription>
             Enter your email below to login to your account.
+            {mutation.isPending && <div>Loading...</div>}
           </CardDescription>
         </CardHeader>
         <CardContent className="grid gap-4">
@@ -76,9 +74,25 @@ const LoginPage = () => {
           </div>
         </CardContent>
         <CardFooter>
-          <Button onClick={handleLoginSubmit} className="w-full">
-            Sign in
-          </Button>
+          
+          <div className="w-full">
+            <Button
+              onClick={handleLoginSubmit}
+              className="w-full"
+              disabled={mutation.isPending}
+            >
+              {mutation.isPending && <LoaderCircle className="animate-spin" />}
+
+              <span className="ml-2">Sign in</span>
+            </Button>
+
+            <div className="mt-4 text-center text-sm">
+              Don't have an account?{" "}
+              <Link to={"/auth/register"} className="underline">
+                Sign up
+              </Link>
+            </div>
+          </div>
         </CardFooter>
         <div className="mb-4 text-center text-m">
           Don't have an account?{" "}
