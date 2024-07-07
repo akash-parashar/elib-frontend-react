3e0c6ffcc96a9dd08fcf6e96e19b4fc03bfef456
akash-parashar <aparashar508@gmail.com>
2024-07-08 01:50:37 +0530

    make routes protected

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom"
+import { Link, Outlet,Navigate} from "react-router-dom"
 import { Button } from "../components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../components/ui/card"
 import {
@@ -18,9 +18,16 @@ import { Badge } from "../components/ui/badge"
 import { Sheet, SheetContent, SheetTrigger } from "../components/ui/sheet"
 import { Input } from "../components/ui/input"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../components/ui/dropdown-menu"
+import useTokenStore from "../store"
   
 
 const DashboardLayout = () => {
+    const token = useTokenStore(state=>state.token);
+    if (token === '') {
+      return <Navigate to={'/auth/login'} replace />;
+  }
+
+
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
